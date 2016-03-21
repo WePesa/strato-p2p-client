e871bd016ba3ed3a2740bccafb6c0aef8afcb2d3
Jamshid <qq@qq.com>
2016-03-20 23:15:39 -0700

    Adding blocknumber of blocks being sent to the output message.

--- src/Blockchain/Display.hs
+++ src/Blockchain/Display.hs
@@ -10,6 +10,7 @@ import Control.Monad.Trans.State
 
 import qualified Blockchain.Colors as CL
 import Blockchain.Context
+import Blockchain.Data.DataDefs
 import Blockchain.Data.Peer
 import Blockchain.Format
 import Blockchain.Data.Wire
@@ -58,8 +59,11 @@ displayMessage _ QqqqPacket = return ()
 displayMessage outbound (BlockHashes shas) = do
   liftIO $ putStrLn $ prefix outbound ++ CL.blue "BlockHashes: " ++ "(" ++ show (length shas) ++ " new hashes)"
   updateStatus
+displayMessage outbound (Blocks []) = do
+  liftIO $ putStrLn $ prefix outbound ++ CL.blue "Blocks: [no blocks]"
+  updateStatus
 displayMessage outbound (Blocks blocks) = do
-  liftIO $ putStrLn $ prefix outbound ++ CL.blue "Blocks: " ++ "(" ++ show (length blocks) ++ " new blocks)"
+  liftIO $ putStrLn $ prefix outbound ++ CL.blue "Blocks: " ++ "(" ++ show (length blocks) ++ " new blocks, ending with #" ++ show (blockDataNumber $ blockBlockData $ last blocks) ++ ")" --last OK because the [] input was pattern matched above
   updateStatus
 displayMessage outbound msg =
   liftIO $ putStrLn $ (prefix outbound) ++ format msg
